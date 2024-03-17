44d4ec0ede17be12a26da9287ef00b9263cb690a
lwalzel <lisa.walzel@hotmail.com>
2024-03-17 17:17:28 +0100

    Improved User Experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Cat poem about ${instructionsInput.value}, Miau!</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
